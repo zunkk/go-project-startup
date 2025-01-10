6041808afd994700fc36722247f2aba2c4fbc877
zunkk <zunkk12345@gmail.com>
2025-01-10 12:32:55 +0800

    feat: opt SQLConnector

--- internal/core/dao/base.go
+++ internal/core/dao/base.go
@@ -2,15 +2,22 @@ package dao
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 
 	"github.com/zunkk/go-project-startup/internal/pkg/base"
 	"github.com/zunkk/go-project-startup/pkg/db/sql"
 	"github.com/zunkk/go-project-startup/pkg/frame"
+	glog "github.com/zunkk/go-project-startup/pkg/log"
 )
+
+var log = glog.WithModule("db")
 
 func init() {
 	frame.RegisterComponents(NewSQLConnector)
 }
+
+type DBAction func(dbTX boil.ContextExecutor) error
 
 type SQLConnector struct {
 	DB *sqlx.DB
@@ -22,4 +29,28 @@ func NewSQLConnector(sidecar *base.CustomSidecar) (*SQLConnector, error) {
 		return nil, err
 	}
 	return &SQLConnector{DB: sqlDB}, nil
+}
+
+func (t *SQLConnector) SubmitDBChangesByTransaction(dbActions ...DBAction) error {
+	dbTX, err := t.DB.Begin()
+	if err != nil {
+		return errors.Wrap(err, "failed to begin db transaction")
+	}
+	// The rollback will be ignored if the tx has been committed later in the function.
+	defer func() {
+		if err != nil {
+			if rollbackErr := dbTX.Rollback(); rollbackErr != nil {
+				log.Warn("Failed to rollback", "err", rollbackErr)
+			}
+		}
+	}()
+	for _, dbAction := range dbActions {
+		if err := dbAction(dbTX); err != nil {
+			return err
+		}
+	}
+	if err := dbTX.Commit(); err != nil {
+		return errors.Wrap(err, "failed to commit db transaction")
+	}
+	return nil
 }
